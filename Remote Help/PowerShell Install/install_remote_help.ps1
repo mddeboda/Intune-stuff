9b12a1076bf7a9aef7f4b8f002e79ecc878cbca8
Mike <76268826+mddeboda@users.noreply.github.com>
2024-03-01 14:37:03 -0500

    Add remote help files upload
    intunewin and powershell script to install Remote Help

---
+++ Remote Help/PowerShell Install/install_remote_help.ps1
@@ -0,0 +1,14 @@
+﻿# Define URL for Remote Help installer
+$installerUrl = "https://aka.ms/downloadremotehelp"
+
+# Specify path where installer will be downloaded
+$installerPath = "C:\MicrosoftRemoteHelpInstaller.exe"
+
+# Download the installer
+Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+
+# Execute installer silently without user interaction
+Start-Process -FilePath $installerPath -Args "/quiet acceptTerms=1 enable AutoUpdates=0" -Wait
+
+# Clean up installer file after install
+Remove-Item -Path $installerPath
